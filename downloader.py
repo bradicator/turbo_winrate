e6542f52ac2c00afdeccbfcd87a0e297c624949c
bradzhao <rfzhao@umich.edu>
2022-04-16 16:35:46 -0500

    added terminal functionalities, rewrote analyzer as class

--- downloader.py
+++ downloader.py
@@ -8,17 +8,27 @@ import requests as _request
 import pandas as _pd
 import json as _json
 from time import sleep as _sleep
-
+from time import ctime as _ctime
+import os as _os
 
 class DataDownloader():
+    
     def __init__(self, sleep_time=1):
         self._sleep_time = sleep_time
         self._lowest_match_id = None
+        self.api_str = None
+        if _os.path.exists("/Users/ruofeizhao/Documents/turbo_winrate/apikey.txt"):
+            with open("/Users/ruofeizhao/Documents/turbo_winrate/apikey.txt") as f:
+                self.api_str = f.readlines()[0].strip('\n')
+        
     
     def get_match_ids(self, match_id_lessthan=None):
         address = "https://api.opendota.com/api/parsedMatches"
         if match_id_lessthan:
-            address += f"?less_than_match_id={match_id_lessthan}"
+            if self.api_str:
+                address += f"?less_than_match_id={match_id_lessthan}&api_key={self.api_str}"
+            else:
+                address += f"?api_key={self.api_str}"        
         _sleep(self._sleep_time)
         response = _request.get(address)
         mlist = _json.loads(response.content)
@@ -26,8 +36,11 @@ class DataDownloader():
         
     def get_raw_match_info(self, match_id):
         _sleep(self._sleep_time)
-        return _request.get(f"https://api.opendota.com/api/matches/{match_id}")
-    
+        if self.api_str:
+            return _request.get(f"https://api.opendota.com/api/matches/{match_id}?api_key={self.api_str}")
+        else: 
+            return _request.get(f"https://api.opendota.com/api/matches/{match_id}")
+        
     def get_parsed_match_info(self, match_id):
         response = self.get_raw_match_info(match_id)
         mdict = _json.loads(response.content)
@@ -41,14 +54,20 @@ class DataDownloader():
         """download matches and write to file, each epoch is 100 matches"""
         with open(fname, mode) as f:
             for e in range(epoch):
+                print(e, _ctime())
                 mids = self.get_match_ids(self._lowest_match_id)
+                buf = ""
+                if e > 0:
+                    print(self._lowest_match_id, all([x < self._lowest_match_id for x in mids]))
                 for id in mids:
                     data = self.get_parsed_match_info(id)
                     if self._lowest_match_id:
                         self._lowest_match_id = min(self._lowest_match_id, data[0])
                     else:
-                        self.lowest_match_id = data[0]
-                    f.write(','.join([str(datum) for datum in data]) + "\n")
+                        self._lowest_match_id = data[0]
+                    buf += ','.join([str(datum) for datum in data]) + "\n"
+                f.writelines(buf)
+                f.flush()
         return
                 
             
--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ author: bradicator
 created on: 2022/04/15
 """
 import json as _json
+import re as _re
 
 def get_heroid_dict(hero_path="./heroinfo.json"):
     with open(hero_path) as f:
@@ -14,4 +15,10 @@ def get_heroid_dict(hero_path="./heroinfo.json"):
        hdict[v['id']] = v['localized_name']
     return hdict
 
- 
+def guess_hero_id(heroname):
+    hdict = get_heroid_dict()
+    lower_hname = heroname.lower()
+    for id, name in hdict.items():
+        if _re.search(lower_hname, name.lower()):
+            return id
+    return None
